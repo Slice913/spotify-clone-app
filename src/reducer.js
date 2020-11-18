1686d9c2fad6e7c87d2fc656f3f29c5113faf12a
Slice <akeemayoade@gmail.com>
2020-11-17 20:31:33 -0600

    first commit for spotify clone

---
+++ src/reducer.js
@@ -0,0 +1,39 @@
+export const initialState = {
+  user: null,
+  playlist: [],
+  playing: false,
+  item: null,
+  // remove after finished after developing
+  // token:
+  // "BQBMR5IS3UzCuKHdAyzmERXxJFPfeOTB--6bXthwchmQVROzXNQVwqBHeLbFczBoL5Fe1WlQs1HJ5mItFNGMv2TISFMVTURhCFlUQEimJwnbT6JnK4RPrw6aMWZ_rzP_bsMyE6KoanlVErmTSyMm92g3KFcqosHFu9ZOrkVRN5-PvOM5E-4n",
+};
+
+const reducer = (state, action) => {
+  console.log(action);
+
+  // Action -> type, [payload]
+
+  switch (action.type) {
+    case "SET_USER":
+      return {
+        ...state,
+        user: action.user,
+      };
+
+    case "SET_TOKEN":
+      return {
+        ...state,
+        token: action.token,
+      };
+
+    case "SET_PLAYLISTS":
+      return {
+        ...state,
+        playlists: action.playlists,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
